bca5186110a89d703634871c488e69ba9c717140
dbpeetz <d.peetz516@gmail.com>
2017-04-07 12:00:43 -0500

    Added Restaurant Class

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restuarants { get; set; }
+    }
+}
